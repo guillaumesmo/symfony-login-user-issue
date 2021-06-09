3392e841187d71081d18dc557f747d71c7b37ed2
gndk <25748293+gndk@users.noreply.github.com>
2021-06-09 12:42:16 +0200

    Add test for checking the session’s security token

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\User\InMemoryUser;
 
 class DefaultControllerTest extends WebTestCase
@@ -19,4 +20,34 @@ class DefaultControllerTest extends WebTestCase
 		$client->request('GET', '/admin-page');
         self::assertResponseStatusCodeSame(403);
 	}
+
+	public function testSessionContainsSecurityToken(): void
+    {
+        $client = self::createClient();
+
+        self::assertUserIsNotAuthenticated();
+
+        $client->loginUser(new InMemoryUser('admin', 'password', ['ROLE_ADMIN']));
+
+        self::assertUserIsAuthenticated();
+    }
+
+    private static function session(): SessionInterface
+    {
+        return static::getContainer()->get(SessionInterface::class);
+    }
+
+    protected static function assertUserIsNotAuthenticated(): void
+    {
+        $token = self::session()->get('_security_main');
+
+        self::assertNull($token, 'The user is authenticated');
+    }
+
+    protected static function assertUserIsAuthenticated(): void
+    {
+        $token = self::session()->get('_security_main');
+
+        self::assertIsString($token, 'The user is not authenticated');
+    }
 }
